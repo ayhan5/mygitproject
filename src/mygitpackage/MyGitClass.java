a0fb18c1db88d451f9e2bc86c97afc8d71e4d220
ayhan5 <hey956@gmail.com>
2021-04-04 12:47:03 -0400

    chng

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -7,12 +7,12 @@ public class MyGitClass {
 		System.out.println("Version2");
 		System.out.println("Version3");
 		System.out.println("changing made in remote repo");// git fetch -> git merge -> git pull
-		System.out.println("2.changing made in ayhan's repo");
+		System.out.println("2.changing made in ayhan's repo");// ayni anda github ta da deg yapildi , yuzden once github taki alinir sonra local deki deg yapilir..ben hata gorunsun diye git ve githubta 10.sati silmedim
 		System.out.println("2.changing made in remote repo");
 		System.out.println("2.changing made in remote repo");
 		
 		
-
+		System.out.println("3.changing made in 15.line remote repo");
 	}
 
 }
