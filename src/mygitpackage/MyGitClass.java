6af03300e59ab2926b2955fd8f412e42ec831f36
ayhan5 <hey956@gmail.com>
2021-04-04 12:08:35 -0400

    10.satirda degisiklik

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -6,8 +6,8 @@ public class MyGitClass {
 		System.out.println("Version1");
 		System.out.println("Version2");
 		System.out.println("Version3");
-		System.out.println("changing made in remote repo");
-		
+		System.out.println("changing made in remote repo");// git fetch -> git merge -> git pull
+		System.out.println("2.changing made in ayhan's repo");
 	}
 
 }
